f99b070c1c9dbe048e8b65feacaa9425a78136c0
程宗鑫 <>
2024-10-12 18:01:24 +0800

    back

---
+++ app/(page)/pageOne.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from '@/components/Themed';
+
+export default function PageOne() {
+  return (
+    <>
+    <Text>123</Text>
+    <Text>123</Text>
+    <Text>123</Text>
+    <Text>123</Text></>
+  );
+}
